8c0f88f4346b93ef257d3f02bc056371da7b6959
Cindy Wu <cindyw44@nycstudents.net>
2026-01-08 14:39:18 -0500

    maybe finished

--- script.js
+++ script.js
@@ -7,31 +7,43 @@ let nebula_spot = 1;
 let comet_spot  = 1;
 
 // Query Selectors
-const apolloBtn   = 
-const nebulaBtn   = 
-const cometBtn    = 
+const apolloBtn = document.querySelector("#apollo-btn");
+const nebulaBtn = document.querySelector("#nebula-btn");
+const cometBtn = document.querySelector("#comet-btn");
 
-const apolloRocket = 
-const nebulaRocket = 
-const cometRocket  = 
-
-const winnerBox = 
+const apolloRocket = document.querySelector("#apollo-rocket");
+const nebulaRocket = document.querySelector("#nebula-rocket");
+const cometRocket  = document.querySelector("#comet-rocket");
 
 // Click Handlers
-const boostApollo = () => {
-  
+const boostApollo = (e) => {
+    apollo_spot += 1;
+    moveTo(apolloRocket, apollo_spot);
+    winnerBox(apollo_spot, "apollo");
 };
 
-const boostNebula = () => {
-
+const boostNebula = (e) => {
+    nebula_spot += 1;
+    moveTo(nebulaRocket, nebula_spot);
+    winnerBox(nebula_spot, "nebula");
 };
 
-const boostComet = () => {
-
+const boostComet = (e) => {
+    comet_spot += 1;
+    moveTo(cometRocket, comet_spot);
+    winnerBox(comet_spot, "comet");
 };
 
 // Winner Check
 
+const winnerBox = (spot, rocket) => {
+    if (spot > 6) {
+        document.getElementById("winner").innerHTML = `<h2>The winner is ${rocket} rocket!</h2>`;
+    }
+}
 
 // Events
+apolloBtn.addEventListener("click", boostApollo);
+nebulaBtn.addEventListener("click", boostNebula);
+cometBtn.addEventListener("click", boostComet);
 
